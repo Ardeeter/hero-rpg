be49fe2023c42acea7434acda5a0b651b34c3985
Ally Deeter <allydeeter6639@gmail.com>
2020-11-19 09:48:44 -0800

    adds step 2

--- hero_rpg.py
+++ hero_rpg.py
@@ -11,10 +11,22 @@ class Hero:
         self.health = health
         self.power = power
 
+    def attack(self, enemy):
+        enemy.health() -= self.power
+        print(f"You do {self.power} damage to the {enemy}.")
+        if enemy_health <= 0:
+            print(f"The {enemy} is dead.")
+
 class Goblin:
     def __init__(self, health, power):
         self.health = health
         self.power = power
+
+def attack(self, hero):
+        hero.health() -= self.power
+        print(f"The goblin does {self.power} damage to you.")
+        if hero_health <= 0:
+            print("You are dead.")
 
 def main():
     # hero_health = 10
@@ -37,10 +49,11 @@ def main():
         raw_input = input()
         if raw_input == "1":
             # Hero attacks goblin
-            goblin.health -= hero.power
-            print("You do {} damage to the goblin.".format(hero.power))
-            if goblin.health <= 0:
-                print("The goblin is dead.")
+            # goblin.health -= hero.power
+            # print("You do {} damage to the goblin.".format(hero.power))
+            # if goblin.health <= 0:
+            #     print("The goblin is dead.")
+            hero.attack(goblin)
         elif raw_input == "2":
             pass
         elif raw_input == "3":
@@ -51,9 +64,10 @@ def main():
 
         if goblin.health > 0:
             # Goblin attacks hero
-            hero.health -= goblin.power
-            print("The goblin does {} damage to you.".format(goblin.power))
-            if hero.health <= 0:
-                print("You are dead.")
+            # hero.health -= goblin.power
+            # print("The goblin does {} damage to you.".format(goblin.power))
+            # if hero.health <= 0:
+            #     print("You are dead.")
+            goblin.atack(hero)
 
 main()
